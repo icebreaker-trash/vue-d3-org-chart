6e8c4a4948d98ae5b21f916ecf3c4f5754aef3f9
ice breaker <1324318532@qq.com>
2022-01-13 20:53:41 +0800

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import typescript from '@rollup/plugin-typescript'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import pkg from './package.json'
+// const isProd = process.env.NODE_ENV === 'production'
+const isDev = process.env.NODE_ENV === 'development'
+
+/** @type {import('rollup').RollupOptions} */
+const config = {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: isDev
+      // exports: 'auto'
+    },
+    { format: 'esm', file: pkg.module, sourcemap: isDev }
+  ],
+
+  plugins: [
+    nodeResolve({
+      preferBuiltins: true
+    }),
+    commonjs(),
+    typescript({ tsconfig: './tsconfig.build.json' })
+  ]
+}
+
+export default config
